027d2ee8fd73925bba2f88055b29081a7f0a5240
LittleFoxyUwU <81238360+LittleFoxyUwU@users.noreply.github.com>
2022-11-23 15:09:55 +0300

    Add sound to beartraps

--- Assets/Scripts/Logic/Traps/BearTrap.cs
+++ Assets/Scripts/Logic/Traps/BearTrap.cs
@@ -6,9 +6,11 @@ namespace Logic.Traps
 {
     public class BearTrap : MonoBehaviour
     {
+        [SerializeField] private AudioSource bearTrapSound;
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.TryGetComponent<PlayerHealth>(out var playerHealth)) return;
+            bearTrapSound.Play();
             playerHealth.GetDamage(50);
             Destroy(gameObject);
         }
