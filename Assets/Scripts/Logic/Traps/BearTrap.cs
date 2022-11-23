ab5f1feaea7e829fc11a4fd6682855df18af4f0e
LittleFoxyUwU <81238360+LittleFoxyUwU@users.noreply.github.com>
2022-11-23 16:17:33 +0300

    Working bear trap

--- Assets/Scripts/Logic/Traps/BearTrap.cs
+++ Assets/Scripts/Logic/Traps/BearTrap.cs
@@ -7,10 +7,13 @@ namespace Logic.Traps
     public class BearTrap : MonoBehaviour
     {
         [SerializeField] private AudioSource bearTrapSound;
+        private static readonly int Close = Animator.StringToHash("Close");
+
         private void OnTriggerEnter(Collider other)
         {
             if (!other.gameObject.TryGetComponent<PlayerHealth>(out var playerHealth)) return;
             bearTrapSound.Play();
+            Destroy(GetComponentInParent<MeshRenderer>());
             playerHealth.GetDamage(50);
             Destroy(gameObject);
         }
