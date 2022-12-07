03ed517e378ab052f8f429c8f30a13a206b55667
LittleFoxyUwU <81238360+LittleFoxyUwU@users.noreply.github.com>
2022-12-07 22:20:30 +0300

    Player attack

--- Assets/GameScripts/Logic/Player/PlayerAttack.cs
+++ Assets/GameScripts/Logic/Player/PlayerAttack.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameScripts.Logic.Enemy;
+using GameScripts.Logic.Player;
 
 public class PlayerAttack : MonoBehaviour
 {
@@ -10,17 +11,20 @@ public class PlayerAttack : MonoBehaviour
 	[SerializeField] private float _attackCooldown=0.6f;
 	[SerializeField] private float _attackRange=2.0f;
 	[SerializeField] private Transform model;
+	[SerializeField] private PlayerAnimator _playerAnimator;
 	
-	private void Update() {
-		if (_canAttack&&Input.GetMouseButton(0)) {
-			var shotDirectrion=model.TransformDirection(Vector3.forward);
-			RaycastHit hit;
-			Physics.Raycast(transform.position, shotDirectrion, out hit, _attackRange);
-			if (hit.collider!=null&&hit.collider.gameObject.layer==8) {
-				hit.transform.GetComponent<EnemyAI>().SetDamage(_damage);
-			}
-			StartCoroutine(AttackCooldown());
+	private void Update()
+	{
+		if (!_canAttack) return;
+		if (!Input.GetMouseButtonDown(0)) return;
+		var shotDirectrion=model.TransformDirection(Vector3.forward);
+		RaycastHit hit;
+		Physics.Raycast(transform.position, shotDirectrion, out hit, _attackRange);
+		if (hit.collider!=null&&hit.collider.gameObject.layer==8) {
+			hit.transform.GetComponent<EnemyAI>().SetDamage(_damage);
 		}
+		_playerAnimator.SetAttack();
+		StartCoroutine(AttackCooldown());
 	}
 	
 	 private IEnumerator AttackCooldown() {
