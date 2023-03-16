8b9bb0f349f1bd485c8d301dee933e33a5b85043
aLBInOS1 <79748635+aLBInOS1@users.noreply.github.com>
2023-03-16 12:42:59 +0300

    Interactable button

--- Assets/GameScripts/StaticData/ScriptableObjects/QuestData.cs
+++ Assets/GameScripts/StaticData/ScriptableObjects/QuestData.cs
@@ -5,6 +5,7 @@ namespace GameScripts.StaticData.ScriptableObjects
     [CreateAssetMenu(fileName = "Quest Data", menuName = "StaticData/QuestData")]
     public class QuestData : ScriptableObject
     {
+        [Header("Current quest")]
         public string questName;
         [TextArea]
         public string description;
@@ -12,6 +13,8 @@ namespace GameScripts.StaticData.ScriptableObjects
         public questImportance questImportance;
         public int value;
         public int reward=100;
+        [Header("Quests branch")]
+        public int additionalQuest = -1;
         public int nextQuest=-1;
     }
 
